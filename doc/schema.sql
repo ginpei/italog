315d972217591b92013f99885d463e5c12306189
Ginpei <ginpei@ginpei.dev>
2024-10-29 23:41:48 -0700

    sql note

---
+++ doc/schema.sql
@@ -0,0 +1,30 @@
+-- create place table
+CREATE TABLE place (
+  id VARCHAR(255) PRIMARY KEY,
+  address VARCHAR(255) NOT NULL,
+  display_name VARCHAR(255) NOT NULL,
+  latitude DOUBLE PRECISION NOT NULL,
+  longitude DOUBLE PRECISION NOT NULL,
+  map_url VARCHAR(255) NOT NULL,
+  type_display_name VARCHAR(255),
+  web_url VARCHAR(255)
+);
+
+-- create profile table
+CREATE TABLE profile (
+  id VARCHAR(255) PRIMARY KEY,
+  display_name VARCHAR(255) NOT NULL
+);
+
+-- create visit table
+CREATE TABLE visit (
+  place_id VARCHAR(255) NOT NULL,
+  user_id VARCHAR(255) NOT NULL,
+  date DATE NOT NULL,
+  comment TEXT NOT NULL,
+  created_at BIGINT NOT NULL,
+  starred BOOLEAN NOT NULL,
+  PRIMARY KEY (place_id, user_id, date),
+  FOREIGN KEY (place_id) REFERENCES place(id),
+  FOREIGN KEY (user_id) REFERENCES profile(id)
+);
