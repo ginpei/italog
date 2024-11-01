4da29c57a71d60b907bf9840b8100d2beb5ab3e5
Ginpei <ginpei@ginpei.dev>
2024-10-31 23:09:04 -0700

    sample data

--- doc/schema.sql
+++ doc/schema.sql
@@ -15,11 +15,19 @@ CREATE TABLE place (
   web_url VARCHAR(255)
 );
 
+-- create sample place
+INSERT INTO place (id, address, display_name, latitude, longitude, map_url, type_display_name, web_url)
+VALUES ('ChIJfe2sYHhxhlQR6YR9clFor1Y', '555 W Hastings St Unit 6, Vancouver, BC V6B 5K3, Canada', 'Tim Hortons', 49.2849465, -123.1119939, 'https://maps.google.com/?cid=6246325907208635625', 'Coffee Shop', 'https://locations.timhortons.ca/en/bc/vancouver/555-hastings-st-west');
+
 -- create profile table
 CREATE TABLE profile (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   display_name VARCHAR(255) NOT NULL
 );
+
+-- create sample profile
+INSERT INTO profile (id, display_name)
+VALUES ('4111179e-3bfa-4b8a-9588-dfe4a49f90bc','DEMO');
 
 -- create auth-profile table
 CREATE TABLE auth_profile (
@@ -42,6 +50,10 @@ CREATE TABLE visit (
   FOREIGN KEY (place_id) REFERENCES place(id),
   FOREIGN KEY (user_id) REFERENCES profile(id)
 );
+
+-- create sample visit
+INSERT INTO visit (comment, created_at, date, place_id, starred, user_id)
+VALUES ('Love it!', 946684800000, '2000-01-01', 'ChIJfe2sYHhxhlQR6YR9clFor1Y', true, '4111179e-3bfa-4b8a-9588-dfe4a49f90bc' );
 
 -- create user relation table
 CREATE TABLE user_user (
