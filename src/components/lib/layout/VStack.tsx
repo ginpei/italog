018699f91084c98b4e389377847d4b76724ddad8
Ginpei <ginpei@ginpei.dev>
2024-10-31 20:36:49 -0700

    accept any tag names

--- src/components/lib/layout/VStack.tsx
+++ src/components/lib/layout/VStack.tsx
@@ -1,15 +1,20 @@
 export interface VStackProps {
+  as?: keyof JSX.IntrinsicElements;
   className?: string;
   children: React.ReactNode;
   gap?: `gap-${4 | 8}`;
 }
 
 export function VStack({
+  as = "div",
   className = "",
   children,
   gap = `gap-4`,
 }: VStackProps): JSX.Element {
+  const TagName = as;
   return (
-    <div className={`${className} VStack flex flex-col ${gap}`}>{children}</div>
+    <TagName className={`${className} VStack flex flex-col ${gap}`}>
+      {children}
+    </TagName>
   );
 }
