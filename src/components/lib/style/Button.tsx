23cabe95da21f63c9001de7a6a72c1112bd31be7
Ginpei <ginpei@ginpei.dev>
2024-10-31 21:28:04 -0700

    disabled style

--- src/components/lib/style/Button.tsx
+++ src/components/lib/style/Button.tsx
@@ -1,4 +1,5 @@
-import { ComponentPropsWithoutRef } from "react";
+import NextLink from "next/link";
+import { ComponentProps, ComponentPropsWithoutRef } from "react";
 
 export function Button({
   className,
@@ -6,6 +7,24 @@ export function Button({
 }: ComponentPropsWithoutRef<"button">): React.JSX.Element {
   return (
     <button
+      className={`
+        Button ${className}
+        border border-gray-400 bg-gray-50 p-2
+        hover:bg-gray-100
+        active:bg-gray-200
+        disabled:bg-gray-300 disabled:text-gray-500
+      `}
+      {...props}
+    />
+  );
+}
+
+export function ButtonLink({
+  className,
+  ...props
+}: ComponentProps<typeof NextLink>): React.JSX.Element {
+  return (
+    <NextLink
       className={`Button ${className} border border-gray-400 bg-gray-50 p-2 hover:bg-gray-100 active:bg-gray-200`}
       {...props}
     />
