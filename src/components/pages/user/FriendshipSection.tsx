627b0939c0b7abd90bf6c577e4dee2b0fad46bd4
Ginpei <ginpei@ginpei.dev>
2024-10-31 22:06:14 -0700

    extract friendship section

---
+++ src/components/pages/user/FriendshipSection.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { Button, ButtonLink } from "@/components/lib/style/Button";
+import { Profile } from "@/components/lib/user/Profile";
+import {
+  createFriendship,
+  deleteFriendship,
+} from "@/components/lib/user/friendshipApi";
+
+export interface FriendshipSectionProps {
+  currentUser: Profile;
+  isFriend: boolean;
+  profile: Profile;
+}
+
+export function FriendshipSection({
+  currentUser,
+  isFriend,
+  profile,
+}: FriendshipSectionProps): JSX.Element {
+  const isOwner = currentUser.id === profile.id;
+
+  const [friendshipWorking, setFriendshipWorking] = useState(false);
+
+  const onSayGoodbyeClick = async () => {
+    const ok = confirm("Are you sure you want to delete friendship statement?");
+    if (!ok) {
+      return;
+    }
+
+    try {
+      setFriendshipWorking(true);
+      await deleteFriendship(profile.id);
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+      setFriendshipWorking(false);
+    }
+  };
+
+  const onBecomeFriendClick = async () => {
+    alert(
+      "[DEV MODE]  Lucky you! 🎉\n\nYou can become a friend with everyone for now",
+    );
+
+    try {
+      setFriendshipWorking(true);
+      await createFriendship(profile.id);
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+      setFriendshipWorking(false);
+    }
+  };
+
+  return (
+    <section className="FriendshipSection text-end">
+      {isOwner ? (
+        <ButtonLink href="/my">My page</ButtonLink>
+      ) : isFriend ? (
+        <Button disabled={friendshipWorking} onClick={onSayGoodbyeClick}>
+          Say goodbye
+        </Button>
+      ) : (
+        <Button disabled={friendshipWorking} onClick={onBecomeFriendClick}>
+          Become a friend
+        </Button>
+      )}
+    </section>
+  );
+}
--- src/components/pages/user/UserPageContent.tsx
+++ src/components/pages/user/UserPageContent.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { FriendshipSection } from "./FriendshipSection";
 import { VStack } from "@/components/lib/layout/VStack";
-import { Button, ButtonLink } from "@/components/lib/style/Button";
 import { H1 } from "@/components/lib/style/Hn";
 import { Profile } from "@/components/lib/user/Profile";
-import {
-  createFriendship,
-  deleteFriendship,
-} from "@/components/lib/user/friendshipApi";
 
 export interface UserPageContentProps {
   currentUser: Profile;
@@ -21,57 +16,14 @@ export function UserPageContent({
   isFriend,
   profile,
 }: UserPageContentProps): JSX.Element {
-  const isOwner = currentUser.id === profile.id;
-
-  const [friendshipWorking, setFriendshipWorking] = useState(false);
-
-  const onSayGoodbyeClick = async () => {
-    const ok = confirm("Are you sure you want to delete friendship statement?");
-    if (!ok) {
-      return;
-    }
-
-    try {
-      setFriendshipWorking(true);
-      await deleteFriendship(profile.id);
-      window.location.reload();
-    } catch (error) {
-      console.error(error);
-      setFriendshipWorking(false);
-    }
-  };
-
-  const onBecomeFriendClick = async () => {
-    alert(
-      "[DEV MODE]  Lucky you! 🎉\n\nYou can become a friend with everyone for now",
-    );
-
-    try {
-      setFriendshipWorking(true);
-      await createFriendship(profile.id);
-      window.location.reload();
-    } catch (error) {
-      console.error(error);
-      setFriendshipWorking(false);
-    }
-  };
-
   return (
     <VStack gap="gap-8">
       <H1>{profile.displayName}</H1>
-      <div className="text-end">
-        {isOwner ? (
-          <ButtonLink href="/my">My page</ButtonLink>
-        ) : isFriend ? (
-          <Button disabled={friendshipWorking} onClick={onSayGoodbyeClick}>
-            Say goodbye
-          </Button>
-        ) : (
-          <Button disabled={friendshipWorking} onClick={onBecomeFriendClick}>
-            Become a friend
-          </Button>
-        )}
-      </div>
+      <FriendshipSection
+        currentUser={currentUser}
+        isFriend={isFriend}
+        profile={profile}
+      />
     </VStack>
   );
 }
