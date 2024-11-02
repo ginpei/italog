cc68a5383e91c24613879c261f1fab9f4ebd6726
Ginpei <ginpei@ginpei.dev>
2024-11-01 18:28:41 -0700

    show skeleton UI

---
+++ src/components/pages/register/PlaceItemSkeleton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function PlaceItemSkeleton(): JSX.Element {
+  return (
+    <div className="flex animate-pulse flex-col gap-2 border border-gray-300 p-2">
+      <div className="h-6 w-1/2 rounded bg-gray-300"></div>
+      <div className="h-4 w-16 rounded bg-gray-300"></div>
+      <div className="h-4 rounded bg-gray-300"></div>
+    </div>
+  );
+}
