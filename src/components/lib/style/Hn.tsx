0856697f6c4e20e2994bfd51c0ba68f921e5715c
Ginpei <ginpei@ginpei.dev>
2024-11-01 18:39:40 -0700

    control styles

--- src/components/lib/style/Hn.tsx
+++ src/components/lib/style/Hn.tsx
@@ -4,19 +4,19 @@ export function H1({
   className,
   ...props
 }: ComponentPropsWithoutRef<"h1">): React.JSX.Element {
-  return <h1 className={`H1 ${className} text-5xl font-bold`} {...props} />;
+  return <h1 className={`${className} H1 text-5xl font-bold`} {...props} />;
 }
 
 export function H2({
   className,
   ...props
 }: ComponentPropsWithoutRef<"h2">): React.JSX.Element {
-  return <h2 className={`H2 ${className} text-3xl font-bold`} {...props} />;
+  return <h2 className={`${className} H2 text-3xl font-bold`} {...props} />;
 }
 
 export function H3({
   className,
   ...props
 }: ComponentPropsWithoutRef<"h3">): React.JSX.Element {
-  return <h3 className={`H3 ${className} text-xl font-bold`} {...props} />;
+  return <h3 className={`${className} H3 text-xl font-bold`} {...props} />;
 }
