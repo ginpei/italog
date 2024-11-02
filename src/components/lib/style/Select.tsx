e60f1ec720053ebce820c5aaec5f25b247e37920
Ginpei <ginpei@ginpei.dev>
2024-11-01 18:40:49 -0700

    select UI

---
+++ src/components/lib/style/Select.tsx
@@ -0,0 +1,32 @@
+import NextLink from "next/link";
+import { ComponentProps, ComponentPropsWithoutRef } from "react";
+
+export function Select({
+  className,
+  ...props
+}: ComponentPropsWithoutRef<"select">): React.JSX.Element {
+  return (
+    <select
+      className={`
+        ${className} Select
+        border border-gray-400 p-2
+        hover:border-gray-500
+        active:bg-gray-200
+        disabled:bg-gray-300 disabled:text-gray-500
+      `}
+      {...props}
+    />
+  );
+}
+
+export function SelectLink({
+  className,
+  ...props
+}: ComponentProps<typeof NextLink>): React.JSX.Element {
+  return (
+    <NextLink
+      className={`Select ${className} border border-gray-400 bg-gray-50 p-2 hover:bg-gray-100 active:bg-gray-200`}
+      {...props}
+    />
+  );
+}
