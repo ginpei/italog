3a7219a8a61063fa14ec6d78e8d4f64de70b96f1
Ginpei <ginpei@ginpei.dev>
2024-11-05 00:29:43 -0800

    trying Quagga

--- src/app/product/search/ProductSearchPageContent.tsx
+++ src/app/product/search/ProductSearchPageContent.tsx
@@ -1,9 +1,63 @@
+"use client";
+
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { BarCodeReader } from "./BarCodeReader";
+import { VStack } from "@/components/layout/VStack";
+import { Button } from "@/components/style/Button";
+import { H1, H2 } from "@/components/style/Hn";
+import { TextInput } from "@/components/style/TextInput";
+
 export interface ProductSearchPageContentProps {
   recentCheckins: never[];
 }
 
 export function ProductSearchPageContent({}: ProductSearchPageContentProps): JSX.Element {
+  const [barcode, setBarcode] = useState("");
+
+  const onBarcodeFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Barcode form submitted");
+  };
+
+  const onBarcodeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (name === "barcode") {
+      setBarcode(value);
+    }
+  };
+
+  const onBarCodeRead = (barcode: string) => {
+    alert(barcode);
+  };
+
   return (
-    <div className="ProductSearchPageContent">ProductSearchPageContent</div>
+    <VStack className="ProductSearchPageContent" gap="gap-8">
+      <H1>Search product</H1>
+      <VStack>
+        <H2>By barcode</H2>
+        <p className="text-sm text-gray-500">
+          Hint: you might want to use speech recognition on your device to enter
+          the barcode by reading it out loud.
+        </p>
+        <form onSubmit={onBarcodeFormSubmit}>
+          <fieldset className="flex items-stretch gap-2">
+            <TextInput
+              className="w-full"
+              inputMode="numeric"
+              onChange={onBarcodeChange}
+              name="barcode"
+              pattern="(\d|\s)*"
+              placeholder="0 00000 00000 0"
+              value={barcode}
+            />
+            <Button>
+              <MagnifyingGlassIcon className="size-5" />
+            </Button>
+          </fieldset>
+        </form>
+        <BarCodeReader onRead={onBarCodeRead} />
+      </VStack>
+    </VStack>
   );
 }
