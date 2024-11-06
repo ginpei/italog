a27f1717d1ec1f8c217074267c96ed3b72dd3558
Ginpei <ginpei@ginpei.dev>
2024-11-05 23:53:09 -0800

    accept error cause option

--- src/components/error/UserError.ts
+++ src/components/error/UserError.ts
@@ -2,8 +2,9 @@
  * Error that users can see safely.
  */
 export class UserError extends Error {
-  constructor(message: string) {
+  constructor(message: string, cause?: unknown) {
     super(message);
     this.name = "UserError";
+    this.cause = cause;
   }
 }
