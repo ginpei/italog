868440801dc26452e07b1d63ed2d0310ccc21e67
Ginpei <ginpei@ginpei.dev>
2024-11-05 19:16:56 -0800

    hide unfinished feature

--- src/app/HomePageContent.tsx
+++ src/app/HomePageContent.tsx
@@ -34,7 +34,7 @@ export function HomePageContent({
                   Place
                 </span>
               </Link>
-              <Link
+              {/* <Link
                 className="grid size-36 items-center justify-center border border-gray-400 bg-gray-50 hover:bg-gray-100 active:bg-gray-200"
                 href="/product/search"
               >
@@ -42,7 +42,7 @@ export function HomePageContent({
                   <ShoppingBagIcon className="mx-auto size-8" />
                   Product
                 </span>
-              </Link>
+              </Link> */}
             </p>
           </VStack>
           <TimelineSection checkins={checkins} />
