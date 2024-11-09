e634b030e6cd679e6a542f3c89ac67c247d3d32a
Ginpei <ginpei@ginpei.dev>
2024-11-08 23:04:50 -0800

    checkin item layout

--- src/components/timeline/BoardCheckinItem.tsx
+++ src/components/timeline/BoardCheckinItem.tsx
@@ -10,21 +10,23 @@ export function BoardCheckinItem({
   checkin,
 }: BoardCheckinItemProps): JSX.Element {
   return (
-    <div className="BoardCheckinItem flex border border-gray-300">
-      <CheckinProfileLink profile={checkin.profile} />
-      <div className="flex flex-col">
-        <span className="text-sm">
-          <Link className="hover:underline" href={`/user/${checkin.userId}`}>
-            {checkin.profile.displayName}
-          </Link>{" "}
-          <span className="text-gray-400">{checkin.userDate}</span>
+    <div className="BoardCheckinItem flex flex-col border border-gray-300 p-2">
+      <div className="flex gap-2">
+        <CheckinProfileLink profile={checkin.profile} />
+        <span className="flex flex-col">
+          <span className="text-sm">
+            <Link className="hover:underline" href={`/user/${checkin.userId}`}>
+              {checkin.profile.displayName}
+            </Link>{" "}
+            <span className="text-gray-400">{checkin.userDate}</span>
+          </span>
+          <span className="text-sm text-gray-400">
+            {checkin.starred && "⭐"}
+            {checkin.board.displayName}
+          </span>
         </span>
-        <span className="text-sm text-gray-400">
-          {checkin.starred && "⭐"}
-          {checkin.board.displayName}
-        </span>
-        <span>{checkin.comment && <> {checkin.comment}</>}</span>
       </div>
+      {checkin.comment && <div className="">{checkin.comment}</div>}
     </div>
   );
 }
