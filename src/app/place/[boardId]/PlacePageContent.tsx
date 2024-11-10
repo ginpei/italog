81ab9595c48ae96b298904a833f9d0d1bf6999f6
Ginpei <ginpei@ginpei.dev>
2024-11-10 02:27:16 -0800

    extract

--- src/app/place/[boardId]/PlacePageContent.tsx
+++ src/app/place/[boardId]/PlacePageContent.tsx
@@ -1,24 +1,16 @@
 "use client";
 
-import {
-  CheckCircleIcon,
-  GlobeAltIcon,
-  MapPinIcon,
-  PencilSquareIcon,
-} from "@heroicons/react/24/outline";
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
-import { APIProvider } from "@vis.gl/react-google-maps";
-import Link from "next/link";
-import { ReactNode, useMemo, useState } from "react";
+import { CheckCircleIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 import { CheckInForm } from "./CheckInForm";
 import { Checkin, CheckinRow } from "@/components/checkin/Checkin";
 import { VStack } from "@/components/layout/VStack";
 import { Place } from "@/components/place/Place";
+import { PlaceDescription } from "@/components/place/PlaceDescription";
 import { requestRegisterVisit } from "@/components/placeCheckin/checkInPlace";
 import { H2 } from "@/components/style/Hn";
 import { BoardCheckinItem } from "@/components/timeline/BoardCheckinItem";
 import { CheckinList } from "@/components/timeline/CheckinList";
-import { EmbeddedMap } from "@/components/timeline/EmbeddedMap";
 
 export interface PlacePageContentProps {
   checkedIn: boolean;
@@ -42,7 +34,6 @@ export function PlacePageContent({
   });
   const [liveCheckins, setUserCheckins] = useState(checkins);
   const [formWorking, setFormWorking] = useState(false);
-  const siteHostName = useHostNameOf(place.webUrl);
 
   const onRegisterCheckinChange = async (checkin: CheckinRow) => {
     setEditingCheckin(checkin);
@@ -91,27 +82,7 @@ export function PlacePageContent({
 
   return (
     <VStack>
-      <hgroup>
-        <h1 className="text-2xl font-bold">
-          {place.displayName || "(No name)"}
-        </h1>
-        <p>{place.typeDisplayName}</p>
-      </hgroup>
-      <div className="h-[30vh] py-1">
-        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-          <EmbeddedMap places={[place]} />
-        </APIProvider>
-      </div>
-      <div className="flex flex-col">
-        <PlaceInfoLink href={place.mapUrl} Icon={MapPinIcon}>
-          {place.address}
-        </PlaceInfoLink>
-        {place.webUrl && (
-          <PlaceInfoLink href={place.webUrl} Icon={GlobeAltIcon}>
-            {siteHostName}{" "}
-          </PlaceInfoLink>
-        )}
-      </div>
+      <PlaceDescription place={place} />
       {/* <hr />
       <RegisterCheckinForm
         disabled={formWorking}
@@ -163,44 +134,4 @@ export function PlacePageContent({
       </CheckinList>
     </VStack>
   );
-}
-
-function PlaceInfoLink({
-  children,
-  href,
-  Icon,
-}: {
-  children: ReactNode;
-  href: string;
-  Icon: React.ForwardRefExoticComponent<
-    React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> & {
-      title?: string;
-      titleId?: string;
-    } & React.RefAttributes<SVGSVGElement>
-  >;
-}): JSX.Element {
-  return (
-    <Link
-      className="flex items-center gap-2 py-2 hover:bg-gray-50 active:bg-gray-100"
-      href={href}
-      target="_blank"
-    >
-      <Icon className="size-8 shrink-0 text-blue-700" />
-      <span>
-        {children}
-        <ArrowTopRightOnSquareIcon className="inline size-4" />
-      </span>
-    </Link>
-  );
-}
-
-function useHostNameOf(url: string | undefined): string | undefined {
-  return useMemo(() => {
-    if (!url) {
-      return undefined;
-    }
-
-    const urlObj = new URL(url);
-    return urlObj.hostname;
-  }, [url]);
 }
