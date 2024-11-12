febed2d0e76dd9cdc6b92cae40d1e4616df1e8d9
Ginpei <ginpei@ginpei.dev>
2024-11-11 22:19:44 -0800

    detect barcode by camera

--- src/app/product/search/ProductSearchPageContent.tsx
+++ src/app/product/search/ProductSearchPageContent.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { CameraIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
-import { BarCodeReader } from "./BarCodeReader";
+import { detectBarcode } from "./quaggaFunctions";
 import { VStack } from "@/components/layout/VStack";
-import { Button } from "@/components/style/Button";
+import { Button, ButtonLabel } from "@/components/style/Button";
 import { H1, H2 } from "@/components/style/Hn";
 import { TextInput } from "@/components/style/TextInput";
 
@@ -17,6 +17,8 @@ export function ProductSearchPageContent({}: ProductSearchPageContentProps): JSX
 
   const onBarcodeFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    // TODO
     console.log("Barcode form submitted");
   };
 
@@ -27,8 +29,29 @@ export function ProductSearchPageContent({}: ProductSearchPageContentProps): JSX
     }
   };
 
-  const onBarCodeRead = (barcode: string) => {
-    setBarcode(barcode);
+  const onBarcodeFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const [file] = event.target.files || [];
+    event.target.value = "";
+    if (!file) {
+      return;
+    }
+
+    const detectedBarcode = await detectBarcode(file, "ean");
+    if (!detectedBarcode) {
+      window.alert("No barcode detected");
+      return;
+    }
+
+    const ok = window.confirm(
+      `Do you want to use this barcode?\n${detectedBarcode}`,
+    );
+    if (!ok) {
+      return;
+    }
+
+    setBarcode(detectedBarcode);
   };
 
   return (
@@ -41,40 +64,43 @@ export function ProductSearchPageContent({}: ProductSearchPageContentProps): JSX
         </p>
         <H1>Search product</H1>
       </VStack>
-      <p className="text-gray-500">
-        You will be able to check in products to share your impression as well
-        as places.
-      </p>
-      <VStack className="hidden">
+      <VStack>
         <H2>By barcode</H2>
         <p className="text-sm text-gray-500">
           Hint: you might want to use speech recognition on your device to enter
           the barcode by reading it out loud.
         </p>
         <form onSubmit={onBarcodeFormSubmit}>
-          <fieldset className="flex items-stretch gap-2" disabled>
-            <TextInput
-              className="w-full"
-              inputMode="numeric"
-              onChange={onBarcodeChange}
-              name="barcode"
-              pattern="(\d|\s)*"
-              placeholder="0 00000 00000 0"
-              value={barcode}
-            />
-            <Button>
-              <MagnifyingGlassIcon className="size-5" />
-            </Button>
-          </fieldset>
+          <VStack>
+            <fieldset className="flex items-stretch gap-2">
+              <TextInput
+                className="w-full"
+                inputMode="numeric"
+                onChange={onBarcodeChange}
+                name="barcode"
+                pattern="(\d|\s)*"
+                placeholder="0 00000 00000 0"
+                value={barcode}
+              />
+              <Button>
+                <MagnifyingGlassIcon className="size-5" />
+              </Button>
+            </fieldset>
+            <div>
+              <ButtonLabel>
+                <input
+                  accept="image/*"
+                  capture
+                  className="hidden"
+                  onChange={onBarcodeFileChange}
+                  type="file"
+                />
+                <CameraIcon className="inline-block size-5" /> Capture barcode
+                (beta)
+              </ButtonLabel>
+            </div>
+          </VStack>
         </form>
-        <details className="w-max border [&[open]]:w-auto">
-          <summary className="cursor-pointer bg-gray-100 p-2">
-            Bar code reader (beta)
-          </summary>
-          <div className="p-2">
-            <BarCodeReader onRead={onBarCodeRead} />
-          </div>
-        </details>
       </VStack>
     </VStack>
   );
