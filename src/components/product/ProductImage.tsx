36ea1f38d2bf13f355124466fa7d646174af44e9
Ginpei <ginpei@ginpei.dev>
2024-11-22 22:47:40 -0800

    show product image

--- src/components/product/ProductImage.tsx
+++ src/components/product/ProductImage.tsx
@@ -1,4 +1,5 @@
 import { PhotoIcon } from "@heroicons/react/24/outline";
+import { controlBorderThemeClassNames } from "../style/controlClassNames";
 
 export interface ProductImageBlockProps {
   imageUrl: string | undefined;
@@ -11,11 +12,16 @@ export function ProductImageBlock({
     // eslint-disable-next-line @next/next/no-img-element
     <img
       alt=""
-      className="ProductImageBlock mx-auto size-64 object-contain"
+      className="ProductImageBlock mx-auto size-64 max-h-full object-contain"
       src={imageUrl}
     />
   ) : (
-    <span className="ProductImageBlock mx-auto grid size-64 place-items-center border text-center">
+    <span
+      className={`ProductImageBlock
+        mx-auto grid size-64 max-h-full place-items-center border text-center
+        ${controlBorderThemeClassNames}
+      `}
+    >
       <span>
         <PhotoIcon className="mx-auto size-32 text-gray-500" />
         (No image)
--- src/components/timeline/TimelineBlock.tsx
+++ src/components/timeline/TimelineBlock.tsx
@@ -1,10 +1,12 @@
 import { APIProvider } from "@vis.gl/react-google-maps";
 import { useRef, useState } from "react";
+import { BoardType } from "../board/Board";
 import { Checkin } from "../checkin/Checkin";
 import { ErrorBlock } from "../error/ErrorBlock";
 import { VStack } from "../layout/VStack";
 import { Place } from "../place/Place";
 import { Product } from "../product/Product";
+import { ProductImageBlock } from "../product/ProductImage";
 import { CheckinList } from "./CheckinList";
 import { EmbeddedMap } from "./EmbeddedMap";
 import { TimelineItem } from "./TimelineItem";
@@ -29,6 +31,12 @@ export function TimelineBlock({
   );
   const placeRefs = useRef<Map<string, HTMLDivElement>>(new Map());
 
+  const primaryBoardType: BoardType = places.some(
+    (v) => v.boardId === primaryBoardId,
+  )
+    ? "place"
+    : "product";
+
   const onMapMarkerClick = (placeId: string) => {
     setPrimaryBoardId(placeId);
     const el = placeRefs.current.get(placeId);
@@ -49,14 +57,14 @@ export function TimelineBlock({
         <div>{/* TODO */}Loading...</div>
       ) : (
         <VStack>
-          <div className="sticky top-0 h-[30vh] bg-white py-1">
-            <TimelinePreviewBlock
-              loading={loading}
-              onMapMarkerClick={onMapMarkerClick}
-              places={places}
-              primaryPlaceId={primaryBoardId}
-            />
-          </div>
+          <TimelinePreviewBlock
+            boardType={primaryBoardType}
+            loading={loading}
+            onMapMarkerClick={onMapMarkerClick}
+            places={places}
+            primaryPlaceId={primaryBoardId}
+            products={products}
+          />
           <CheckinList>
             {checkins.map((checkin) => (
               <div
@@ -81,23 +89,29 @@ export function TimelineBlock({
 }
 
 interface TimelinePreviewBlockProps {
+  boardType: BoardType;
   loading: boolean;
   onMapMarkerClick: (id: string) => void;
   places: Place[];
   primaryPlaceId: string;
+  products: Product[];
 }
 
 function TimelinePreviewBlock({
+  boardType,
   loading,
   onMapMarkerClick,
   places,
   primaryPlaceId,
+  products,
 }: TimelinePreviewBlockProps): JSX.Element {
+  const product = products.find((p) => p.boardId === primaryPlaceId);
+
   return (
-    <div className="TimelinePreviewBlock contents">
+    <div className="TimelinePreviewBlock sticky top-0 h-[30vh] bg-white py-1 dark:bg-black">
       {loading ? (
-        <div className="size-full animate-pulse bg-gray-300" />
-      ) : (
+        <div className="size-full animate-pulse" />
+      ) : boardType === "place" ? (
         <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
           <EmbeddedMap
             onPlaceClick={onMapMarkerClick}
@@ -105,6 +119,13 @@ function TimelinePreviewBlock({
             primaryPlaceId={primaryPlaceId}
           />
         </APIProvider>
+      ) : (
+        <div className="relative h-full">
+          <ProductImageBlock imageUrl={product!.imageUrl} />
+          <div className="absolute bottom-0 w-full bg-gray-500/80 p-2 text-white">
+            {product!.displayName}
+          </div>
+        </div>
       )}
     </div>
   );
